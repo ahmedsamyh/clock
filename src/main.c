d26032a2ff4a26400d2e625fe7786a9f5c44b416
ahmedsamyh <ahmedsamyh10@gmail.com>
2024-03-15 00:09:32 +0500

    Update clock_init() api

--- src/main.c
+++ src/main.c
@@ -1,1 +1,20 @@
-#include "../examples/drawing_sprite.c"
+#include <clock/clock.h>
+
+int main(void) {
+  Context* ctx = clock_init(800, 800, "Clock");
+
+  if (ctx == NULL) {
+    return 1;
+  }
+
+  while (!clock_should_quit(ctx)) {
+    clock_begin_draw(ctx);
+    clock_clear(ctx, COLOR_BLACK);
+
+    clock_end_draw(ctx);
+  }
+
+  clock_deinit(ctx);
+
+  return 0;
+}
